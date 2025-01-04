5da117e008f80b6baa92fed8feb658dea74ab385
Nazmul Joy <Nazmuljoys@gmail.com>
2025-01-04 13:01:06 +0600

    4jan25

---
+++ vue-project4/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+   
+
+    <div class="wrapper">
+
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink> &nbsp;
+        <RouterLink to="/food">Food Item</RouterLink> &nbsp;
+        <RouterLink to="/todo">ToDo</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project4/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import FoodItem from '@/views/FoodItem.vue'
+import Todolist from '@/views/Todolist.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/food',
+      name: 'Food',
+      component: FoodItem,
+    },
+    {
+      path: '/todo',
+      name: 'ToDo',
+      component: Todolist,
+    },
+  ],
+})
+
+export default router
